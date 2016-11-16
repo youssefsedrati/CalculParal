0fa22be399c35eac50ab8fee62cf1b763c1c42ae
WINDPASSINGER Marcel <mwindpassin@cocagne.pedago.ipb.fr>
2016-11-16 16:52:47 +0100

    Jacobi method without parallelism

--- testC/jacobi.h
+++ testC/jacobi.h
@@ -1,7 +1,7 @@
 #ifndef JACOBI_H
 #define JACOBI_H
 
-void Jacobi(int maxiter,int eps,double Aii,double Cx,double Cy,int Nx,int N,double *RHS,double *U, double *Uold);
+void Jacobi(int maxiter,int eps,double Aii,double Cx,double Cy,int Nx,int N,double *RHS,double *U);
 
 
 #endif
